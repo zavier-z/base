8c7edd95f4be717498b5d363e3c0c812028fe1e0
zhangwen.zavier <zhangwen.zavier@bytedance.com>
2022-11-08 13:30:57 +0800

    [common] result

--- common/error.h
+++ common/error.h
@@ -3,6 +3,8 @@
 #include <optional>
 #include <string>
 
+#include "check.h"
+
 namespace base {
 
 class Error {
@@ -18,12 +20,18 @@ class Error {
   static constexpr std::uint32_t kNoErrorCode = 0;
 
  public:
+  Error() : code_(kNoErrorCode), category_(nullptr), message_() {}
   Error(const Category* category, std::uint32_t code)
       : code_(code), category_(category), message_() {}
   Error(const Category* category, std::uint32_t code, const std::string& msg)
       : code_(code), category_(category), message_(msg) {}
   Error(const Category* category, std::uint32_t code, std::string&& msg)
       : code_(code), category_(category), message_(std::move(msg)) {}
+
+  Error(Error&&) = default;
+  Error(const Error&) = default;
+  Error& operator=(Error&&) = default;
+  Error& operator=(const Error&) = default;
 
  public:
   std::uint32_t code() const { return code_; }
@@ -32,22 +40,34 @@ class Error {
 
   bool Has() const { return category_ != nullptr; }
   operator bool() const { return Has(); }
-  bool HasMessage() const { return message_.has_value(); }
-  // TODO: maybe core
-  std::string GetMessage() const { return message_.value(); }
-  std::string PassMessage() { return std::move(*message_); }
 
-  void Clear() {
-    category_ = nullptr;
-    message_.reset();
-    code_ = kNoErrorCode;
-  }
+  bool HasMessage() const { return message_.has_value(); }
+  inline std::string PassMessage();
+  inline std::string GetMessage() const;
+
+  inline void Clear();
 
  private:
   std::uint32_t code_;
   const Category* category_;
   std::optional<std::string> message_;
 };
+
+inline void Error::Clear() {
+  category_ = nullptr;
+  message_.reset();
+  code_ = kNoErrorCode;
+}
+
+inline std::string Error::GetMessage() const {
+  DCHECK(HasMessage());
+  return message_.value();
+}
+
+inline std::string Error::PassMessage() {
+  DCHECK(HasMessage());
+  return std::move(*message_);
+}
 
 }  // namespace base
 
---
+++ common/result.h
@@ -0,0 +1,115 @@
+#pragma once
+
+#include <optional>
+#include <variant>
+
+#include "check.h"
+#include "error.h"
+
+namespace base {
+
+template <typename T>
+class Result {
+ public:
+  Result() : v_() {}
+  Result(const T& ok) : v_(ok) {}
+  Result(T&& ok) : v_(std::move(ok)) {}
+
+  Result(const Error& err) : v_(err) {}
+  Result(Error&& err) : v_(std::move(err)) {}
+
+  Result(Result&&) = default;
+  Result(const Result&) = default;
+
+  Result& operator=(Result&&) = default;
+  Result& operator=(const Result&) = default;
+
+ public:
+  bool IsEmpty() const { return v_.index() == kEmpty; }
+  bool IsOk() const { return v_.index() == kOk; }
+  bool IsError() const { return v_.index == kError; }
+  operator bool() const { return IsOk(); }
+
+  void Clear() { v_ = Null{}; }
+
+ public:
+  T PassResult();
+  const T& GetResult() const;
+
+  Error PassError();
+  const Error& GetError() const;
+
+ private:
+  struct Null {};
+  enum { kEmpty, kOk, kError };
+  std::variant<Null, T, Error> v_;
+};
+
+template <>
+class Result<void> {
+ public:
+  Result() : v_() {}
+  Result(const Error& err) : v_(err) {}
+  Result(Error&& err) : v_(std::move(err)) {}
+
+  Result(Result&&) = default;
+  Result(const Result&) = default;
+
+  Result& operator=(Result&&) = default;
+  Result& operator=(const Result&) = default;
+
+ public:
+  bool IsEmpty() const { return false; }
+  bool IsOk() const { return static_cast<bool>(!v_); }
+  bool IsError() const { return !IsOk(); }
+  operator bool() const { return IsOk(); }
+
+  void Clear() { v_.Clear(); }
+
+ public:
+  Error PassError() { return std::move(v_); }
+  const Error& GetError() const { return v_; }
+
+ private:
+  Error v_;
+};
+
+template <typename T>
+T Result<T>::PassResult() {
+  auto ptr = std::get_if<T>(&v_);
+
+  DCHECK(ptr);
+  auto result = std::move(*ptr);
+
+  Clear();
+  return result;
+}
+
+template <typename T>
+const T& Result<T>::GetResult() const {
+  auto ptr = std::get_if<T>(&v_);
+
+  DCHECK(ptr);
+  return *ptr;
+}
+
+template <typename T>
+Error Result<T>::PassError() {
+  auto ptr = std::get_if<Error>(&v_);
+
+  DCHECK(ptr);
+  auto error = std::move(*ptr);
+
+  Clear();
+  return error;
+}
+
+template <typename T>
+const Error& Result<T>::GetError() const {
+  auto ptr = std::get_if<Error>(&v_);
+
+  DCHECK(ptr);
+  return *ptr;
+}
+
+}  // namespace base
