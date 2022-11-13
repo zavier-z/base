25a7918df384d9747f9cdac9cda0740669949a68
zavier <zhangwen.zavier@bytedance.com>
2022-11-13 23:48:15 +0800

    [event]

--- common/common.h
+++ common/common.h
@@ -1,9 +1,11 @@
 #pragma once
 
-#include <cstdlib>
 #include <cstdarg>
-#include <string>
+#include <cstdlib>
 #include <exception>
+#include <string>
+
+#include "check.h"
 
 namespace base {
 
@@ -26,6 +28,98 @@ R ExceptionRun(F&& h, const char* expr, const char* file, int line) {
     Die("%s.%d]: %s thrown unknown exception", file, line, expr);
   }
 }
+
+class NonCopyable {
+ public:
+  NonCopyable(const NonCopyable&) = delete;
+  NonCopyable& operator=(const NonCopyable&) = delete;
+
+ protected:
+  NonCopyable() = default;
+  ~NonCopyable() = default;
+};
+
+template <typename T>
+void DestoryInplace(T& ref) noexcept {
+  ref.~T();
+}
+
+template <typename T, typename... ARGS>
+void ConstructInplace(T& ref, ARGS&&... args) noexcept {
+  ::new (std::addressof(ref)) T(std::forward(args)...);
+}
+
+template <typename T>
+class ObjectInplace final : NonCopyable {
+ public:
+  ObjectInplace() = default;
+  ~ObjectInplace() noexcept {
+    if (initialize_) {
+      DestoryInplace(*ptr());
+    }
+  }
+
+  ObjectInplace& operator=(T&& value) noexcept {
+    ConstructInplace(*ptr(), std::move(value));
+    initialize_ = true;
+    return *this;
+  }
+
+  ObjectInplace& operator=(const T& value) noexcept {
+    DCHECK(!initialize_);
+    ConstructInplace(*ptr(), value);
+    initialize_ = true;
+    return *this;
+  }
+
+ public:
+  T& operator*() noexcept {
+    DCHECK(initialize_);
+    return *ptr();
+  }
+
+  const T& operator*() const noexcept {
+    DCHECK(initialize_);
+    return *ptr();
+  }
+
+ private:
+  T* ptr() noexcept { return reinterpret_cast<T*>(&data_); }
+  const T* ptr() const noexcept { return reinterpret_cast<const T*>(&data_); }
+
+ private:
+  std::aligned_storage<sizeof(T), alignof(T)> data_;
+  bool initialize_{false};
+};
+
+template <typename T>
+class ObjectInplace<T&> final : NonCopyable {
+ public:
+  ObjectInplace() = default;
+  ~ObjectInplace() = default;
+
+  ObjectInplace& operator=(T& value) noexcept {
+    DCHECK(!initialize_);
+    ptr_ = &value;
+    initialize_ = true;
+    return *this;
+  }
+
+ public:
+  T& operator*() noexcept {
+    DCHECK(initialize_);
+    return *ptr_;
+  }
+
+  const T& operator*() const noexcept {
+    DCHECK(initialize_);
+    return *ptr_;
+  }
+
+ private:
+  T* ptr_{nullptr};
+  bool initialize_{false};
+};
 
 }  // namespace _
 }  // namespace base
---
+++ event/executor.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <functional>
+
+namespace event {
+
+// |Executor| provides the execution environment for function/callback. the
+// underlaying implementation maybe athread pool and so on
+class Executor {
+ public:
+  virtual ~Executor() {}
+
+  // run a function/callback on appropriate time
+  virtual void Post(std::function<void()>&&) = 0;
+};
+
+class LocalExecutor : public Executor {
+ private:
+  // the function/callback is called in place
+  void Post(std::function<void()>&& f) override { f(); }
+};
+
+}  // namespace event
